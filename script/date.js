034e8d5f34ac81730f65cc0449296fbbf2bc2092
lillyasquith <lydang26993@gmail.com>
2023-04-27 21:34:27 -0600

    Update the copyright year in JS with querySelector

--- script/date.js
+++ script/date.js
@@ -1,3 +1,5 @@
 const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" };
+const currentYear = new Date;
 document.getElementById("currentdate").textContent = new Date().toLocaleTimeString("en-US", options);
-alert(document.lastModified);
+document.querySelector("#year").textContent = currentYear.getFullYear();
+// alert(document.lastModified); // No need to include the alert in JavaScript for the last modified date

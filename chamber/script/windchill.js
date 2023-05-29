41e28ff77b4b2d3df59f7b921ad4856fdac87f3d
lillyasquith <lydang26993@gmail.com>
2023-05-29 16:13:50 -0600

    completed Responsive Images Activity

---
+++ chamber/script/windchill.js
@@ -0,0 +1,4 @@
+// get the two input values of temperature and wind speed,
+// check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+// either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+// Wind Chill Specification Limits: "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)."

dc3caece8e30c0c5ef4d192c51a1903776e65191
lillyasquith <lydang26993@gmail.com>
2023-05-30 16:49:35 -0600

    added windchill js

--- chamber/script/windchill.js
+++ chamber/script/windchill.js
@@ -1,4 +1,32 @@
-// get the two input values of temperature and wind speed,
-// check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
-// either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
-// Wind Chill Specification Limits: "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)."
+//Get the two input values of temperature and wind speed
+let speedElement = document.getElementById("speed");
+const speed = 5;
+speedElement.innerHTML = `${speed} mph`;
+console.log(speed);
+
+let tempElement = document.getElementById("temp");
+const temp = 50;
+tempElement.innerHTML = `${temp} °F`;
+
+//Compute the windchill
+let windchill =
+  35.74 +
+  0.6215 * temp -
+  35.75 * Math.pow(speed, 0.16) +
+  0.4275 * temp * Math.pow(speed, 0.16);
+
+//Round the answer down to interger
+windchill = Math.round(windchill);
+
+//Check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+if (temp <= 50 && speed > 3.0) {
+  document.querySelector("#wc").textContent = `${windchill}° F`;
+  // OR this is another way to display the windchill to console
+  //  const wc = document.querySelector("#wc");
+  //  console.log(windchill);
+  //  wc.innerHTML = `${windchill}°F`;
+}
+//either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+else {
+  document.querySelector("#wc").textContent = "N/A";
+}

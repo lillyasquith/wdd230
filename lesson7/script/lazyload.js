9863920e96cefd4098fede3fd637b0d8ac2335fb
lillyasquith <lydang26993@gmail.com>
2023-06-01 23:44:48 -0600

    add scale hover img

--- lesson7/script/lazyload.js
+++ lesson7/script/lazyload.js
@@ -1,18 +1,9 @@
-//get all the imgs with data-src attribute
 const images = document.querySelectorAll("[data-src");
 
-//optional parameters being set for the IntersectionObserver
 const imgOptions = {
   threshold: 1,
   rootMargin: "0px 0px 50px 0px",
 };
-
-// const loadImages = (image) => {
-//   image.setAttribute("src", image.getAttribute("data-src"));
-//   image.onload = () => {
-//     image.removeAttribute("data-src");
-//   };
-// };
 
 function preloadImg(img) {
   const src = img.getAttribute("data-src");
@@ -21,27 +12,6 @@ function preloadImg(img) {
   }
   img.src = src;
 }
-//first check to see if the IntersectionObserver is supported
-
-// if ("IntersectionObserver" in window) {
-//   const imgObserver = new IntersectionObserver((items, imgObserver ) => {
-//     items.forEach((item) => {
-//       if (item.isIntersecting) {
-//         loadImages(item.target);
-//         imgObserver.unobserve(item.target);
-//       }
-//     });
-//   }, imgOptions);
-//   //loop through each img and check status and load if necessary
-//   imagesToLoad.forEach((img) => {
-//     imgObserver.observe(img);
-//   });
-// } else {
-//   // just load all imgs if not supported
-//   imagesToLoad.forEach((img) => {
-//     loadImages(img);
-//   });
-// }
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {

7a7b7bb969c3e13ebfff50f2ee7f695591611372
lillyasquith <lydang26993@gmail.com>
2023-06-04 19:30:41 -0600

    lazy loading worked properly

---
+++ chamber/script/lazy.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll("[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 100px 0px", //top, right, bottom, lelf
+};
+
+function preloadImg(img) {
+  const src = img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = () => {
+    img.removeAttribute("data-src");
+  };
+}
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImg(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
+});

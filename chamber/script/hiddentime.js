7780118277769750cd1f1c74dd78f97069471191
lillyasquith <lydang26993@gmail.com>
2023-06-10 17:10:54 -0600

    join page in progress

---
+++ chamber/script/hiddentime.js
@@ -0,0 +1,11 @@
+//A hidden field that contains the current date and time that the form was loaded by the user
+
+var d = new Date();
+// Set the value of the "date" field
+document.getElementById("date").value = d.toDateString();
+
+// Set the value of the "time" field
+var hours = d.getHours();
+var mins = d.getMinutes();
+var seconds = d.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;

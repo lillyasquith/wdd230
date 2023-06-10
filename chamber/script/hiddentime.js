a5879137d0c42644785f1ed54e4195556639da72
lillyasquith <lydang26993@gmail.com>
2023-06-10 17:19:44 -0600

    validated join html

--- chamber/script/hiddentime.js
+++ chamber/script/hiddentime.js
@@ -2,10 +2,10 @@
 
 var d = new Date();
 // Set the value of the "date" field
-document.getElementById("date").value = d.toDateString();
+document.getElementById("datenow").value = d.toDateString();
 
 // Set the value of the "time" field
 var hours = d.getHours();
 var mins = d.getMinutes();
 var seconds = d.getSeconds();
-document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+document.getElementById("timenow").value = hours + ":" + mins + ":" + seconds;

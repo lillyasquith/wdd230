377d6f0fa0ff03b7a44ece8560101a1ba4f2015e
lillyasquith <lydang26993@gmail.com>
2023-06-19 06:41:16 -0600

    directory in progress

--- chamber/script/directory.js
+++ chamber/script/directory.js
@@ -1,18 +1,42 @@
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-  // example using arrow function
-  display.classList.add("grid");
-  display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-  display.classList.add("list");
-  display.classList.remove("grid");
+const url = "https://lillyasquith.github.io/wdd230/chamber/data.json";
+async function getDirectoryData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  //   console.table(data.companies);
+  displayDirectory(data.companies);
 }
+const displayDirectory = (companies) => {
+  const cards = document.querySelector("div.cards"); // select the output container element
+
+  companies.forEach((business) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement("section");
+    let picture = document.createElement("img");
+    let name = document.createElement("h3");
+    let location = document.createElement("p");
+    let contact = document.createElement("p");
+    let site = document.createElement("a");
+
+    name.textContent = business.name;
+    location.textContent = business.address;
+    contact.textContent = business.phone;
+    site.textContent = business.website;
+    site.setAttribute("href", business.website);
+
+    picture.setAttribute("src", business.logo);
+    picture.setAttribute("loading", "lazy");
+    picture.setAttribute("width", "200");
+    picture.setAttribute("height", "100");
+    picture.style.objectFit = "cover";
+    // Append the section(card) with the created elements//the order from 1st to last of these Element in html start with name and end with site od each card.
+    card.appendChild(name);
+    card.appendChild(picture);
+    card.appendChild(location);
+    card.appendChild(contact);
+    card.appendChild(site);
+
+    cards.appendChild(card);
+  }); // end of forEach loop
+}; // end of function expression
+
+getDirectoryData();

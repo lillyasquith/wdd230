84254a1b4caf8f48ea0645e4cbadd45723b58ad8
lillyasquith <lydang26993@gmail.com>
2023-06-23 11:52:00 -0600

    W10 Activity: Weather API

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,26 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+//create an "url" variable using const that stores the API call
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q={Fairbanks}&units={imperial}&appid={c997a331d2044e07373c41f6ea65c0c4}";
+
+//write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();

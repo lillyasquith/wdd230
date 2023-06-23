43a1610b3d6660eccb181e70f82f86439aab20c1
lillyasquith <lydang26993@gmail.com>
2023-06-23 16:44:27 -0600

    weather API completed

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -5,7 +5,7 @@ const captionDesc = document.querySelector("figcaption");
 
 //create an "url" variable using const that stores the API call
 const url =
-  "https://api.openweathermap.org/data/2.5/weather?q={Fairbanks}&units={imperial}&appid={c997a331d2044e07373c41f6ea65c0c4}";
+  "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4";
 
 //write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
 async function apiFetch() {
@@ -14,7 +14,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // this is for testing the call
-      // displayResults(data);
+      displayResults(data);
     } else {
       throw Error(await response.text());
     }
@@ -22,5 +22,14 @@ async function apiFetch() {
     console.log(error);
   }
 }
+function displayResults(weatherData) {
+  currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
 
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", "desc");
+  captionDesc.textContent = desc;
+}
 apiFetch();

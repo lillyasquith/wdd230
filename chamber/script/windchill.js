13711030fccf26e53bfe0cde19da6160ebb77ba3
lillyasquith <lydang26993@gmail.com>
2023-06-24 18:44:30 -0600

    Weather API in progress

--- chamber/script/windchill.js
+++ chamber/script/windchill.js
@@ -1,12 +1,12 @@
 //Get the two input values of temperature and wind speed
-let speedElement = document.getElementById("speed");
-const speed = 5;
-speedElement.innerHTML = `${speed} mph`;
-console.log(speed);
+// let speedElement = document.getElementById("speed");
+// const speed = 5;
+// speedElement.innerHTML = `${speed} mph`;
+// console.log(speed);
 
-let tempElement = document.getElementById("temp");
-const temp = 50;
-tempElement.innerHTML = `${temp} °F`;
+// let tempElement = document.getElementById("temp");
+// const temp = 50;
+// tempElement.innerHTML = `${temp} °F`;
 
 //Compute the windchill
 let windchill =
@@ -30,3 +30,42 @@ if (temp <= 50 && speed > 3.0) {
 else {
   document.querySelector("#wc").textContent = "N/A";
 }
+
+//WEATHER API
+// select HTML elements in the document
+const currentTemp = document.querySelector("#temp");
+const currentSpeed = document.querySelector("#speed");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherDesc = document.querySelector("figcaption");
+
+//create an "url" variable using const that stores the API call
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Tooele&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4";
+
+//write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+function displayResults(weatherData) {
+  currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+  currentSpeed.innerHTML = `${weatherData.wind.speed.toFixed(1)} mph`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", "desc");
+  weatherDesc.textContent = desc;
+}
+apiFetch();

6ce77fd1361f8be8feff2dbb6ee517df879586ed
lillyasquith <lydang26993@gmail.com>
2023-06-28 11:13:28 -0600

    adjust visitcount

--- chamber/script/visitcount.js
+++ chamber/script/visitcount.js
@@ -1,12 +1,12 @@
 //get the stored value in localStorage
-const lastVisit = localStorage.getItem("lastvisit");
+const lastVisit = Number(window.localStorage.getItem("lastvisit"));
 // console.log(lastVisit);
 
 const currentDate = new Date();
 const previousVisit = new Date(lastVisit);
 const timeDiff = Math.abs(currentDate.getTime() - previousVisit.getTime());
 // console.log(timeDiff);
-const daysSinceVisit = Math.floor(timeDiff / (60 * 60 * 24 * 1000));
+const daysSinceVisit = Math.round(timeDiff / (60 * 60 * 24 * 1000));
 console.log(daysSinceVisit);
 
 document.querySelector("#dayspass").innerHTML = daysSinceVisit;

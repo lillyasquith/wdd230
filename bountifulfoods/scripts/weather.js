ece61c359230cae6d044a92086adc4679f5b4dbd
lyasquith <lydang26993@gmail.com>
2023-07-20 09:41:03 -0600

    update changes

--- bountifulfoods/scripts/weather.js
+++ bountifulfoods/scripts/weather.js
@@ -7,13 +7,12 @@ const currentSpeed = document.querySelector("#speed");
 const humidity = document.querySelector("#humid");
 
 //create an "url" variable using const that stores the API call
-const url =
+const weatherUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat=37.93836&lon=-121.71035&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4";
-
-//write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
+  //write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
 async function apiFetch() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(weatherUrl);
     if (response.ok) {
       const data = await response.json();
       //console.log(data); // this is for testing the call
@@ -53,3 +52,67 @@ const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satur
 let d = new Date();
 let now = d.getDay();
 datefield.textContent = days[now];
+
+
+// (3) day temperature forecast
+const forecastUrl = 
+  "https://api.openweathermap.org/data/2.5/forecast?lat=37.93836&lon=-121.71035&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4"
+async function fetchForecastData() {
+  try {
+    const response = await fetch(forecastUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data)
+      //showWeatherdata(data.forecastData);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchForecastData();
+
+const forecastcards = document.getElementById("cards");
+function showWeatherdata(forecastData) {
+  forecastcards.innerHTML = "";
+
+  let filteredEl = forecastData.filter((weather) => 
+  weather.temp && weather.humidity && weather.speed);
+
+  filteredEl = filteredEl.slice(0,3);
+
+  filteredEl.forEach ((card) => {
+    let unit = document.createElement("div");
+    let icon = document.createElement("img");
+    let temp = document.createElement("h2");
+    let desc = document.createElement("figcaption")
+    let windspeed = document.createElement("p");
+    let humidity = document.createElement("p");
+
+    temp.innerHTML = `<strong>${card.main.temp.toFixed(
+      0
+    )}</strong>`;
+    windspeed.innerHTML = `<strong>${card.wind.speed.toFixed(
+      0
+    )}</strong> mph`;
+    humidity.innerHTML = card.main.humidity.toFixed(
+      0
+    )
+    const iconsrc = `https://openweathermap.org/img/w/${card.weather[0].icon}.png`;
+    const description = card.weather[0].description;
+  
+    icon.setAttribute("src", iconsrc);
+    icon.setAttribute("alt", "desc");
+    desc.textContent = description;
+  
+    unit.appendChild(icon);
+    unit.appendChild(temp);
+    unit.appendChild(desc);
+    unit.appendChild(windspeed);
+    unit.appendChild(humidity);
+  
+    forecastcards.appendChild(unit);
+
+  });
+}

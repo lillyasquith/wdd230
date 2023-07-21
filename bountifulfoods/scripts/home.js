cf65e4508bfcb9185a75b7b70a0e97f5e2a10968
lyasquith <lydang26993@gmail.com>
2023-07-20 22:11:55 -0600

    update changes

--- bountifulfoods/scripts/home.js
+++ bountifulfoods/scripts/home.js
@@ -18,3 +18,15 @@ document.getElementById("currentdate").textContent = new Date().toLocaleTimeStri
 
 const currentYear1 = new Date();
 document.querySelector("#year").textContent = currentYear1.getFullYear();
+
+// DISPLAY NUMBER OF DRINKS IN THE HOME PAGE
+const displayDrinks = document.querySelector("#total-drinks");
+let numberOfDrinks = Number(window.localStorage.getItem("numDrinks-ls")) || 0;
+
+if (displayDrinks !== null) {
+  if (numberOfDrinks !== 0) {
+    displayDrinks.innerHTML = numberOfDrinks;
+  } else {
+    displayDrinks.innerHTML = "You have not made any orders yet.";
+  }
+}

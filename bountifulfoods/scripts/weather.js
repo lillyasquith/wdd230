6e17a911cbdef0101612f6f159d3f11cb6cb2186
lyasquith <lydang26993@gmail.com>
2023-07-22 00:31:11 -0600

    update changes

--- bountifulfoods/scripts/weather.js
+++ bountifulfoods/scripts/weather.js
@@ -1,49 +1,46 @@
 //WEATHER API
 // select HTML elements in the document
-const currentTemp = document.querySelector("#temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const weatherDesc = document.querySelector("figcaption");
-const currentSpeed = document.querySelector("#speed");
-const humidity = document.querySelector("#humid");
+// const currentTemp = document.querySelector("#temp");
+// const weatherIcon = document.querySelector("#weather-icon");
+// const weatherDesc = document.querySelector("figcaption");
+// const currentSpeed = document.querySelector("#speed");
+// const humidity = document.querySelector("#humid");
 
-//create an "url" variable using const that stores the API call
-const weatherUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat=37.93836&lon=-121.71035&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4";
-  //write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
-async function apiFetch() {
-  try {
-    const response = await fetch(weatherUrl);
-    if (response.ok) {
-      const data = await response.json();
-      //console.log(data); // this is for testing the call
-      displayResults(data);
-    } else {
-      throw Error(await response.text());
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-apiFetch();
+// //create an "url" variable using const that stores the API call
+// const weatherUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?lat=37.93836&lon=-121.71035&units=imperial&appid=c997a331d2044e07373c41f6ea65c0c4";
+//   //write a defined asynchronous function that will use fetch() to request the given weather api url and then try to convert the response using a JSON parser that is built-in
+// async function apiFetch() {
+//   try {
+//     const response = await fetch(weatherUrl);
+//     if (response.ok) {
+//       const data = await response.json();
+//       //console.log(data); // this is for testing the call
+//       displayResults(data);
+//     } else {
+//       throw Error(await response.text());
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// apiFetch();
 
-function displayResults(weatherData) {
-  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  currentSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong> mph`;
-  humidity.innerHTML = weatherData.main.humidity.toFixed(0);
+// function displayResults(weatherData) {
+//   currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+//   currentSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong> mph`;
+//   humidity.innerHTML = weatherData.main.humidity.toFixed(0);
 
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const description = weatherData.weather[0].description;
+//   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+//   const description = weatherData.weather[0].description;
 
-  weatherIcon.setAttribute("src", iconsrc);
-  weatherIcon.setAttribute("alt", "desc");
-  weatherDesc.textContent = description;
-}
+//   weatherIcon.setAttribute("src", iconsrc);
+//   weatherIcon.setAttribute("alt", "desc");
+//   weatherDesc.textContent = description;
+// }
 
-// Select the DOM elements to manipulate
-const datefield = document.getElementById("today");
-// derive the current date using a date object
+const datefield = document.querySelector(".today");
 let date = new Date();
-const currentDate = new Date();
 let day = date.toLocaleString('en-us', {weekday: 'long'});
 datefield.textContent = day;
 
@@ -56,7 +53,7 @@ async function fetchForecastData() {
     const response = await fetch(forecastUrl);
     if (response.ok) {
       const data = await response.json();
-      showWeatherdata(data.forecastData);
+      showWeatherdata(data);
     } else {
       throw Error(await response.text());
     }
@@ -65,48 +62,29 @@ async function fetchForecastData() {
   }
 }
 fetchForecastData();
+function showWeatherdata(weatherData) {
 
-const forecastcards = document.getElementById("cards");
+  const forecastData = weatherData.list;
 
-function showWeatherdata(forecastData) {
-  forecastcards.innerHTML = "";
+  for (let i = 1; i <= 3; i++) {
 
-  let filteredEl = forecastData.filter((weather) => 
-  weather.temp && weather.humidity && weather.speed);
+    const dayData = forecastData[i * 8 - 1];
 
-  filteredEl = filteredEl.slice(0,3);
+    const temp = dayData.main.temp.toFixed(0);
 
-  filteredEl.forEach ((card) => {
-    let unit = document.createElement("div");
-    let icon = document.createElement("img");
-    let temp = document.createElement("h2");
-    let desc = document.createElement("figcaption")
-    let windspeed = document.createElement("p");
-    let humidity = document.createElement("p");
+    const desc = dayData.weather[0].description;
 
-    temp.innerHTML = `<strong>${card.main.temp.toFixed(
-      0
-    )}</strong>`;
-    windspeed.innerHTML = `<strong>${card.wind.speed.toFixed(
-      0
-    )}</strong> mph`;
-    humidity.innerHTML = card.main.humidity.toFixed(
-      0
-    )
-    const iconsrc = `https://openweathermap.org/img/w/${card.weather[0].icon}.png`;
-    const description = card.weather[0].description;
-  
-    icon.setAttribute("src", iconsrc);
-    icon.setAttribute("alt", "desc");
-    desc.textContent = description;
-  
-    unit.appendChild(icon);
-    unit.appendChild(temp);
-    unit.appendChild(desc);
-    unit.appendChild(windspeed);
-    unit.appendChild(humidity);
-  
-    forecastcards.appendChild(unit);
+    const iconsrc = `https://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
 
-  });
+    document.querySelector(`#day${i}-temp`).textContent = temp + "°F";
+
+    document.querySelector(`#day${i}-figcaption`).textContent = desc;
+
+    document.querySelector(`#day${i}-icon`).setAttribute("src", iconsrc);
+
+    document.querySelector(`#day${i}-icon`).setAttribute("alt", desc);
+
+  }
+
 }
+

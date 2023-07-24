ae7ac954556b928d5fc1e69870a52d77017cfb55
lyasquith <lydang26993@gmail.com>
2023-07-24 07:41:08 -0600

    update changes

--- bountifulfoods/scripts/fresh.js
+++ bountifulfoods/scripts/fresh.js
@@ -33,3 +33,74 @@ const displayFruits = (fruits) => {
   });
 };
 
+let dateNow = new Date();
+let lastModified = dateNow[document.lastModified];
+
+const btn = document.querySelector(".submit-Btn");
+
+const firstName = document.getElementById("f-name");
+const out1 = document.getElementById("nameF");
+
+const lastName = document.getElementById("l-name");
+const out2 = document.getElementById("nameL");
+
+const phone = document.getElementById("p-num");
+const out3 = document.getElementById("phone-number");
+
+const email = document.getElementById("e-mail");
+const out4 = document.getElementById("my-email");
+
+const fruit1 = document.getElementById("option-1");
+const out5 = document.getElementById("fruit1");
+
+const fruit2 = document.getElementById("option-2");
+const out6 = document.getElementById("fruit2");
+
+const fruit3 = document.getElementById("option-3");
+const out7 = document.getElementById("fruit3");
+
+const quantity = document.getElementById("quantity");
+const out8 = document.getElementById("total-drinks");
+
+const instructions = document.getElementById("message");
+const out9 = document.getElementById("text-output");
+
+const date = document.getElementById("date-time");
+const out10 = document.getElementById("time-output");
+
+function outputs() {
+  out1.innerHTML= firstName.value;
+  out2.innerHTML= lastName.value;
+  out3.innerHTML= phone.value;
+  out4.innerHTML= email.value;
+  out5.innerHTML= fruit1.value;
+  out6.innerHTML= fruit2.value;
+  out7.innerHTML= fruit3.value;
+  out8.innerHTML= quantity.value;
+  out9.innerHTML= instructions.value;
+  out10.innerHTML= new Intl.DateTimeFormat("en-GB", options).format(
+    lastModified
+  );
+
+  // Clear input fields after submission
+  firstName.value = "";
+  lastName.value = "";
+  phone.value = "" ;
+  email.value = "";
+  fruit1.value = "";
+  fruit2.value = "";
+  fruit3.value = "";
+  quantity.value = "";
+  instructions.value = "";
+  date.value = "";
+  
+  let numDrinks = Number(window.localStorage.getItem("numDrinks-ls")) || 0;
+    // increment the number of drinks.
+  numDrinks += quantityValue;
+    // store the new number of drinks total into localStorage, key=numDrinks-ls
+  localStorage.setItem("numDrinks-ls", `${numDrinks}`);
+
+}
+btn.addEventListener("click",outputs);
+ 
+
